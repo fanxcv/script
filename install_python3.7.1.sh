ffd74e132dd691692339e0ae488cb2787c3f038e
f763180872 <763180872@qq.com>
2019-12-07 20:50:31 +0800

    add sqlite-devel

--- install_python3.7.1.sh
+++ install_python3.7.1.sh
@@ -4,7 +4,7 @@ cd /usr/local/python3tmp
 wget https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tgz
 tar xvf Python-3.7.1.tgz
 cd Python-3.7.1
-yum -y install gcc zlib-devel openssl-devel libffi-devel
+yum -y install gcc zlib-devel openssl-devel libffi-devel sqlite-devel
 ./configure --prefix=/usr/local/python3
 make
 make install
