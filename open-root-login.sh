b997e0cdae185292f5372642c0b3a717b6907176
f763180872 <763180872@qq.com>
2021-07-09 09:47:21 +0800

    Update open-root-login.sh

--- open-root-login.sh
+++ open-root-login.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo root:$1 | sudo chpasswd root
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo service sshd restart
+#echo root:$1 | sudo chpasswd root
+#sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+#sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+#sudo service sshd restart
 
 #关闭oracle监控工具
 systemctl stop oracle-cloud-agent
